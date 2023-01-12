9f3b710fdc9aa8f0b9d648542d3ee37d404f6ce1
SAMYAK BHABHRA <1ds20is090@dsce.edu.in>
2023-01-12 13:09:35 +0530

    Add files via upload

---
+++ reverseWordbyWord.java
@@ -0,0 +1,48 @@
+package Strings;
+//{ Driver Code Starts
+    import java.util.*;
+    import java.lang.*;
+    import java.io.*;
+    class reverseWordbyWord {
+        public static void main(String[] args) {
+    
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while (t > 0) {
+                String s = sc.next();
+                Solution obj = new Solution();
+                System.out.println(obj.reverseWords(s));
+                t--;
+            }
+        }
+    }
+    
+    // } Driver Code Ends
+    
+    
+    
+    class Solution 
+    {
+        //Function to reverse words in a given string.
+        String reverseWords(String S)
+        {
+           StringTokenizer ss = new StringTokenizer(S,".");
+    
+            StringBuilder r = new StringBuilder();
+    
+            
+    
+            while (ss.hasMoreTokens()) {
+    
+                r.insert(0,ss.nextToken()+".");
+    
+            }
+    
+            r.deleteCharAt(S.length());
+    
+            
+    
+            return r.toString();
+    
+        }
+    }
